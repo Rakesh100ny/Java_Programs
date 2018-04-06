ab4eb05836e5e02bb460b129d2cb3b92606a0e26
Ranu100ny <baberwalrakesh@yahoo.com>
2018-04-06 21:00:56 +0530

    toDO

--- toDO_app/service/readJson.js
+++ toDO_app/service/readJson.js
@@ -2,8 +2,6 @@ app.service('readJson',function($http){
   this.getJson=function(){
     return $http.get("products.json").then(function(response){
       return response.data;
-      console.log(response.data);
-      
     });
   }
 });
