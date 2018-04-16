a466ec8d46822ec3a47a4e20c9303d7697752ace
Ranu100ny <baberwalrakesh@yahoo.com>
2018-04-16 21:15:42 +0530

    toDO

--- toDO_app/app.js
+++ toDO_app/app.js
@@ -1,42 +1,36 @@
-var app=
-angular.module('myApp',['ui.router','ngMaterial','ngMessages','angular.filter']);
-app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider)
-{
-$stateProvider
+var app =
+  angular.module('myApp', ['ui.router', 'ngMaterial', 'ngMessages', 'angular.filter']);
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
-.state('test',
-{
-  url         : '/test',
-  templateUrl : 'templates/test.html',
-  controller  : 'mainController'
-})
+    .state('test', {
+      url: '/test',
+      templateUrl: 'templates/test.html',
+      controller: 'mainController'
+    })
 
-.state('login',
-{
-  url         : '/login',
-  templateUrl : 'templates/login.html',
-  controller  : 'loginCtrl'
-})
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
 
-.state('home',
-{
-  url         : '/home',
-  templateUrl : 'templates/home.html',
-  controller  : 'homeCtrl'
-})
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html',
+      controller: 'homeCtrl'
+    })
 
-.state('readJson',
-{
-  url         : '/readJson',
-  templateUrl : 'templates/readingData.html',
-  controller  : 'jsonCtrl'
-})
+    .state('readJson', {
+      url: '/readJson',
+      templateUrl: 'templates/readingData.html',
+      controller: 'jsonCtrl'
+    })
 
-.state('home.dashboard',
-{
-  url         : '/dashboard',
-  templateUrl : 'templates/dashboard.html',
-  controller  : 'dashboardCtrl'
-});
+    .state('home.dashboard', {
+      url: '/dashboard',
+      templateUrl: 'templates/dashboard.html',
+      controller: 'dashboardCtrl'
+    });
   $urlRouterProvider.otherwise('/login');
 }]);
